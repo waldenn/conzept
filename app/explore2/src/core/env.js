2d1bb42c76f68d92b788e28bd1ba3ab3a2d3ef41
waldenn <poulalma@gmail.com>
2022-01-01 19:28:17 +0100

    cover fix

--- app/explore2/src/core/env.js
+++ app/explore2/src/core/env.js
@@ -1,4 +1,4 @@
-const CONZEPT_VERSION = "0.50.007";
+const CONZEPT_VERSION = "0.50.008";
 const CONZEPT_WEB_BASE = "";
 const CONZEPT_CORS_PROXY_WHITELIST = "upload.wikimedia.org, commons.wikimedia.org, commons.m.wikimedia.org, maps.wikimedia.org";
 const CONZEPT_HOSTNAME = "conze.pt";
