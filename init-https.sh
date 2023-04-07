88b54ea3b6b2d7b472b1759e5722acc32499d7ee
Brahim Afassy <brahimafassy@gmail.com>
2023-04-07 21:51:31 +0000

    ssl for production by default

--- init-https.sh
+++ init-https.sh
@@ -13,7 +13,7 @@
   data_path="./data/certbot"
     # => $CONZEPT_EMAIL
   email="$CONZEPT_EMAIL" # Adding a valid address is strongly recommended
-  staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+  staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
   if [ -d "$data_path" ]; then
     read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
