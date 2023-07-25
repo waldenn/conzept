aa97cb972df1565d975f6f9586dbb65be7bb5ef2
waldenn <poulalma@gmail.com>
2023-07-25 15:01:22 +0200

    fix .php

--- init-https.sh
+++ init-https.sh
@@ -4,17 +4,14 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
 fi
-# TODO - Dynamic Domain from Env Variables
 
 . "$PWD/settings.conf"
 
-# => $CONZEPT_HOSTNAME
 domains="$CONZEPT_HOSTNAME"
+email="$CONZEPT_EMAIL"
+staging="$CONZEPT_STAGING" # Set to 1 if you're testing your setup to avoid hitting request limits
 rsa_key_size=4096
 data_path="./data/certbot"
-  # => $CONZEPT_EMAIL
-email="$CONZEPT_EMAIL" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
