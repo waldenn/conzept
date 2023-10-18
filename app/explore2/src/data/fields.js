e1a1b230d79417a18617ea90c4a4e1ec8cf3dfb3
waldenn <poulalma@gmail.com>
2023-10-18 20:41:39 +0200

    author fields

--- app/explore2/src/data/fields.js
+++ app/explore2/src/data/fields.js
@@ -8749,6 +8749,7 @@ if ( valid( item.found_in_taxon ) ){
   rank: [51,1640],
 },
 
+/*
 'written_works_query' : {
   create_condition: 'valid( item.is_author ) && checkTag(item, 0, "person")',
   title: 'written works',
@@ -8761,16 +8762,16 @@ if ( valid( item.found_in_taxon ) ){
   section: ['art','main'],
   rank: [410, 1720],
 },
+*/
 
-/*
 'author_works_query' : {
   create_condition: 'valid( item.is_author ) && checkTag(item, 0, "person")',
   title: 'author works query',
   prop: '',
   type: 'link-split',
   mv: false,
-  url: '${explore.base}/app/query/embed.html?l=${explore.language}#SELECT%20DISTINCT%20%3Fbook%20%3FbookLabel%20%3Fseries%20%3FseriesLabel%20(MIN(%3FpublicationDate)%20AS%20%3Fpublication)%20(GROUP_CONCAT(%3Fgenre_label%3B%20SEPARATOR%20%3D%20%22%2C%20%22)%20AS%20%3Fgenres)%20%3FformLabel%20%3Fimg%20WHERE%20%7B%0A%20%20%3Fbook%20wdt%3AP50%20wd%3A${item.qid.%0A%20%20OPTIONAL%20%7B%0A%20%20%20%20%3Fbook%20wdt%3AP136%20%3Fgenre.%0A%20%20%20%20%3Fgenre%20rdfs%3Alabel%20%3Fgenre_label.%0A%20%20%20%20FILTER((LANG(%3Fgenre_label))%20%3D%20%22${explore.language}%22)%0A%20%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP577%20%3FpublicationDate.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP18%20%3Fimg.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP7937%20%3Fform.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP179%20%3Fseries.%20%7D%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%2${explore.language}%2Cen%2Cceb%2Csv%2Cde%2Cfr%2Cnl%2Cru%2Cit%2Ces%2Cpl%2Cwar%2Cvi%2Cja%2Czh%2Carz%2Car%2Cuk%2Cpt%2Cfa%2Cca%2Csr%2Cid%2Cno%2Cko%2Cfi%2Chu%2Ccs%2Csh%2Cro%2Cnan%2Ctr%2Ceu%2Cms%2Cce%2Ceo%2Che%2Chy%2Cbg%2Cda%2Cazb%2Csk%2Ckk%2Cmin%2Chr%2Cet%2Clt%2Cbe%2Cel%2Caz%2Csl%2Cgl%2Cur%2Cnn%2Cnb%2Chi%2Cka%2Cth%2Ctt%2Cuz%2Cla%2Ccy%2Cta%2Cvo%2Cmk%2Cast%2Clv%2Cyue%2Ctg%2Cbn%2Caf%2Cmg%2Coc%2Cbs%2Csq%2Cky%2Cnds%2Cnew%2Cbe-tarask%2Cml%2Cte%2Cbr%2Ctl%2Cvec%2Cpms%2Cmr%2Csu%2Cht%2Csw%2Clb%2Cjv%2Csco%2Cpnb%2Cba%2Cga%2Cszl%2Cis%2Cmy%2Cfy%2Ccv%2Clmo%2Cwuu%2Cbn%22.%20%7D%0A%7D%0AGROUP%20BY%20%3Fbook%20%3FbookLabel%20%3FauthorLabel%20%3Fsubtitle%20%3FformLabel%20%3Fseries%20%3FseriesLabel%20%3Fimg%0AORDER%20BY%20DESC%20(%20%3Fpublication)%0A%23meta%3Aauthor%20works%20${item.title}',
-  icon: 'fa-solid fa-book-open-reader',
+  url: '${explore.base}/app/query/embed.html?l=${explore.language}#SELECT%20DISTINCT%20%3Fbook%20%3FbookLabel%20%3Fseries%20%3FseriesLabel%20(MIN(%3FpublicationDate)%20AS%20%3Fpublication)%20(GROUP_CONCAT(%3Fgenre_label%3B%20SEPARATOR%20%3D%20%22%2C%20%22)%20AS%20%3Fgenres)%20%3FformLabel%20%3Fimg%20WHERE%20%7B%0A%20%20%3Fbook%20wdt%3AP50%20wd%3A${item.qid}.%0A%20%20OPTIONAL%20%7B%0A%20%20%20%20%3Fbook%20wdt%3AP136%20%3Fgenre.%0A%20%20%20%20%3Fgenre%20rdfs%3Alabel%20%3Fgenre_label.%0A%20%20%20%20FILTER((LANG(%3Fgenre_label))%20%3D%20%22${explore.language}%22)%0A%20%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP577%20%3FpublicationDate.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP18%20%3Fimg.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP7937%20%3Fform.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fbook%20wdt%3AP179%20%3Fseries.%20%7D%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%2${explore.language}%2Cen%2Cceb%2Csv%2Cde%2Cfr%2Cnl%2Cru%2Cit%2Ces%2Cpl%2Cwar%2Cvi%2Cja%2Czh%2Carz%2Car%2Cuk%2Cpt%2Cfa%2Cca%2Csr%2Cid%2Cno%2Cko%2Cfi%2Chu%2Ccs%2Csh%2Cro%2Cnan%2Ctr%2Ceu%2Cms%2Cce%2Ceo%2Che%2Chy%2Cbg%2Cda%2Cazb%2Csk%2Ckk%2Cmin%2Chr%2Cet%2Clt%2Cbe%2Cel%2Caz%2Csl%2Cgl%2Cur%2Cnn%2Cnb%2Chi%2Cka%2Cth%2Ctt%2Cuz%2Cla%2Ccy%2Cta%2Cvo%2Cmk%2Cast%2Clv%2Cyue%2Ctg%2Cbn%2Caf%2Cmg%2Coc%2Cbs%2Csq%2Cky%2Cnds%2Cnew%2Cbe-tarask%2Cml%2Cte%2Cbr%2Ctl%2Cvec%2Cpms%2Cmr%2Csu%2Cht%2Csw%2Clb%2Cjv%2Csco%2Cpnb%2Cba%2Cga%2Cszl%2Cis%2Cmy%2Cfy%2Ccv%2Clmo%2Cwuu%2Cbn%22.%20%7D%0A%7D%0AGROUP%20BY%20%3Fbook%20%3FbookLabel%20%3FauthorLabel%20%3Fsubtitle%20%3FformLabel%20%3Fseries%20%3FseriesLabel%20%3Fimg%0AORDER%20BY%20DESC%20(%20%3Fpublication)%0A%23meta%3Aauthor%20works%20${item.title}',
+  icon: 'fa-solid fa-pen-fancy',
   text: 'author works',
   section: ['library-general','art','main'],
   rank: [15,20,1600],
@@ -8778,7 +8779,6 @@ if ( valid( item.found_in_taxon ) ){
   headline_type: 'link-split',
   headline_rank: 145,
 },
-*/
 
 'religion' : {
   title: 'faith',
