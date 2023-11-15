d2b233831d1e170e87063b42d77f938e65f9c5c9
waldenn <poulalma@gmail.com>
2023-11-15 22:21:59 +0100

    video app language

--- app/video/build.sh
+++ app/video/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd scripts &&
 cat app.js controllers/list.js controllers/view.js services/youtube.js filters/htmlify.js directives/scroll.js > ../bundle.js
-#cd .. && minify bundle_.js > bundle.js && rm bundle_.js
