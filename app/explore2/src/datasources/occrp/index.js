9f57d01408f3051b4ccee165c27ad0831e2fc27c
waldenn <poulalma@gmail.com>
2024-04-04 17:33:00 +0200

    working OCCRP

--- app/explore2/src/datasources/occrp/index.js
+++ app/explore2/src/datasources/occrp/index.js
@@ -61,7 +61,7 @@ function processResultsOCCRP( topicResults, struct, index ){
 
   return new Promise(( resolve, reject ) => {
 
-    console.log( topicResults );
+    //console.log( topicResults );
 
     if ( !valid( topicResults.results ) ){
 
@@ -80,7 +80,7 @@ function processResultsOCCRP( topicResults, struct, index ){
     }
     else {
 
-      datasources[ source ].total = topicResults.meta.count;
+      datasources[ source ].total = topicResults.total;
 
       let result = {
 
@@ -137,7 +137,7 @@ function processResultsOCCRP( topicResults, struct, index ){
 
         if ( valid( obj.schema ) ){
 
-          if ( Array.isArray( item.schema ) ){
+          if ( Array.isArray( obj.schema ) ){
 
             console.log( 'TODO: handle this OCCRP list type: ', obj.schema );
 
@@ -146,6 +146,10 @@ function processResultsOCCRP( topicResults, struct, index ){
 
             if ( obj.schema.toLowerCase() === 'person' ){
               maintag = 'person';
+            }
+            else if ( obj.schema.toLowerCase() === 'company' ){
+              maintag = 'organization';
+              subtag = 'company';
             }
             else if ( obj.schema.toLowerCase() === 'organization' ){
               maintag = 'organization';
@@ -210,9 +214,11 @@ function processResultsOCCRP( topicResults, struct, index ){
 
         }
 
+        desc = desc + `<a target="_blank" title="source" aria-label="source" href="${url}">source</a><br/><br/>`;
+
         if ( valid( obj?.collection?.label ) ){
 
-            desc += label;
+            desc += obj.collection.label;
 
         }
 
@@ -224,7 +230,7 @@ function processResultsOCCRP( topicResults, struct, index ){
 
 						Object.keys( countries ).forEach( ( qid ) => {
 
-							if ( countries[ qid ].iso2 === iso ){
+							if ( countries[ qid ].iso2 === iso.toUpperCase() ){
 
 								country_qids.push( qid );
 
@@ -236,15 +242,14 @@ function processResultsOCCRP( topicResults, struct, index ){
 
         }
 
-       	addItemCountries( item, country_qids, false );
-
         // fill fields
 				let item = {
           source:       source,
 					title:        title,
 					description:  desc,
 					gid:          gid,
-					display_url:  url,
+          web_url:      url,
+					display_url:  '', // not viewable, due to CORS restriction
 					thumb:        '',
           start_date:   start_date,
 					qid:          '',
@@ -252,10 +257,12 @@ function processResultsOCCRP( topicResults, struct, index ){
           tags:         [],
 				};
 
-				item.tags[0]	= maintag;
-				item.tags[1]	= subtag;
+       	addItemCountries( item, country_qids, false );
 
 				setWikidata( item, [ ], true, 'p' + explore.page );
+
+				item.tags[0]	= maintag;
+				item.tags[1]	= subtag;
 
         result.source.data.query.search.push( item ); 
 
