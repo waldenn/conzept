2bfc85376434747bd0d6c703cb9f84d7f114a3c0
waldenn <poulalma@gmail.com>
2024-05-03 12:49:15 +0200

    fullscreen-key UV app, force showing Safari scrollbar, audio app stying

--- app/explore2/libs/entitree_fullscreen.js
+++ app/explore2/libs/entitree_fullscreen.js
@@ -1,5 +1,7 @@
 document.addEventListener('keydown', function(event) {
 
+  event.preventDefault();
+
   if ( event.key === 'f' ) {
 
     const fullscreenButton = document.getElementById( 'fullscreen-button' );
