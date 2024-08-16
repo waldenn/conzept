598fd0baa6344f2182403d694d702d1ddb5da32d
waldenn <poulalma@gmail.com>
2024-08-16 16:39:25 +0200

    contrain video to Wikipedia + Wikidata

--- app/explore2/src/data/fields.js
+++ app/explore2/src/data/fields.js
@@ -13437,7 +13437,7 @@ if ( valid( item.found_in_taxon ) ){
 },
 
 'video_headline' : { // only used for headline display
-  create_condition: true,
+  create_condition: 'activeOnDatasources( [ "wikipedia", "wikidata" ], item.datasource )',
   title: '',
   prop: '',
   type: 'link',
@@ -13448,7 +13448,7 @@ if ( valid( item.found_in_taxon ) ){
   section: '',
   rank: 1,
   //TODO?: ( tags[0] === 'work' && ! valid( item.is_written_work ) && !valid( item.openlibrary_id ) && tags[1] !== 'periodical' ) // show video for works, but not for some types of works
-  headline_create: 'checkTag( item, 0, ["location","time","organization","group","organism","person","natural-type","substance","cultural-concept","natural-concept","meta-concept"] ) || checkTag( item, 1, ["geographical-structure","religion","museum","video-game","music","symbol","art","film","tv-series","filmmaker","actor","architect","musician","music-instrument","software","monument"] )',
+  headline_create: 'checkTag( item, 0, ["location","time","organization","group","organism","person","natural-type","substance","cultural-concept","natural-concept","meta-concept"] ) || checkTag( item, 1, ["geographical-structure","religion","museum","video-game","music","symbol","art","film","tv-series","filmmaker","actor","architect","musician","music-instrument","software","artificial-intelligence","monument"] )',
   headline_type: 'link',
   headline_title: 'video',
   headline_icon: 'fa-solid fa-video',
@@ -13457,7 +13457,7 @@ if ( valid( item.found_in_taxon ) ){
 },
 
 'topical_video_stream' : { // FIXME not rendering for raw-search-strings, why?
-  create_condition: true,
+  create_condition: 'activeOnDatasources( [ "wikipedia", "wikidata" ], item.datasource )',
   title: 'topical YouTube video stream',
   prop: '',
   type: 'wander',
@@ -13466,7 +13466,7 @@ if ( valid( item.found_in_taxon ) ){
   text: 'stream',
   section: ['media-video','main'],
   rank: [30, 30],
-  headline_create: 'checkTag( item, 0, ["location","time","organization","group","organism","person","natural-type","substance","cultural-concept","natural-concept","meta-concept"] ) || checkTag( item, 1, ["geographical-structure","religion","museum","video-game","music","symbol","art","film","tv-series","filmmaker","actor","architect","musician","music-instrument","software","monument"] )', // checkPersona( [ "student", "streamer" ] ) 
+  headline_create: 'checkTag( item, 0, ["location","time","organization","group","organism","person","natural-type","substance","cultural-concept","natural-concept","meta-concept"] ) || checkTag( item, 1, ["geographical-structure","religion","museum","video-game","music","symbol","art","film","tv-series","filmmaker","actor","architect","musician","music-instrument","software","artificial-intelligence","monument"] )', // checkPersona( [ "student", "streamer" ] ) 
   headline_type: 'wander',
   headline_icon: 'fa-solid fa-tv',
   headline_rank: 200.1,
