b7c8ceee041fd526e1b3786ba41d7ca7057c9573
waldenn <poulalma@gmail.com>
2024-09-19 00:22:56 +0200

    Sketchfab paging cursor

--- app/explore2/src/data/datasources.js
+++ app/explore2/src/data/datasources.js
@@ -1773,7 +1773,7 @@ const datasources = {
                               'entity'          : '',
                             },
                             // TODO: "published_since": "Retrieves the models published after published_since (ISO 8601 format)" 
-    url:                    '${datasources.sketchfab.endpoint}?tags=${ term.toLowerCase().replaceAll( " ", "," ) }&count=${datasources.sketchfab.pagesize}&cursor=${datasources.sketchfab.cursor}',
+    url:                    '${datasources.sketchfab.endpoint}?q=${term}&count=${datasources.sketchfab.pagesize}&cursor=${datasources.sketchfab.cursor}',
     cursor:                 '',
     icon:                   '<img class="datasource-icon" alt="Sketchfab logo" src="/assets/icons/sketchfab.svg" alt="Sketchfab logo">',
     icon_invert:            false,
@@ -1785,7 +1785,7 @@ const datasources = {
     code_render_mark:       'renderMarkSketchfab( inputs, source, q_, show_raw_results, id )',
     autocomplete_active:    true,
     autocomplete_protocol:  'json',
-    autocomplete_url:       '${datasources.sketchfab.endpoint}?tags=${ term.toLowerCase().replaceAll( " ", "," ) }&count=${datasources.sketchfab.autocomplete_limit}',
+    autocomplete_url:       '${datasources.sketchfab.endpoint}?q=${term}&count=${datasources.sketchfab.autocomplete_limit}',
     autocomplete_format:    'json',
     autocomplete_connect:   'json',
     autocomplete_limit:     `${explore.datasource_autocomplete_limit}`,
